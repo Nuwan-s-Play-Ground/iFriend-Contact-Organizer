1ef7596bcc5035f0c4f359fbe4facb4b08618e2e
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-06-04 16:13:18 +0530

    Add clearConsole method

--- Friend.java
+++ Friend.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 class Friend{
 
+
+    //----------------------CLEAR CONSOLE --------------------
+    public final static void clearConsole() { 
+        try {
+        final String os = System.getProperty("os.name"); 
+        if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } else {
+        System.out.print("\033[H\033[2J"); 
+        System.out.flush();
+        }
+        } catch (final Exception e) {
+        e.printStackTrace();
+        // Handle any exceptions.
+        }
+    }
+
     public static void main(String[] args) {
         homePage();
     }
